c9829eff381953dd7de53b36cc344f937ba0416e
Ovyerus <ovy@ovyerus.com>
2023-11-16 23:45:56 +1100

    gfh: fix description

--- Formula/gfh.rb
+++ Formula/gfh.rb
@@ -1,7 +1,7 @@
 class Gfh < Formula
-  desc "A CLI batch downloader for your Bandcamp collection."
+  desc "Sign your Git commits with multiple resident SSH keys"
   homepage "https://github.com/Ovyerus/gfh"
-  url "https://github.com/ovyerus/gfh/archive/v0.0.4.tar.gz"
+  url "https://github.com/ovyerus/gfh/archive/refs/tags/v0.0.4.tar.gz"
   sha256 "1167ca9ce0073799d45d46e729517ddef3cf0adaa184e37eab85501a326d25fb"
   license "MIT"
   head "https://github.com/Ovyerus/gfh.git", branch: "main"
