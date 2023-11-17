395eaaeb8c6015b0a380f51eb9ecad3774c6308f
Ovyerus <ovy@ovyerus.com>
2023-11-18 00:01:08 +1100

    gfh: fix test

--- Formula/gfh.rb
+++ Formula/gfh.rb
@@ -17,7 +17,7 @@ class Gfh < Formula
     output = shell_output("#{bin}/gfh --help")
     assert_match "Git FIDO helper - use multiple FIDO keys to sign Git commits", output
 
-    output2 = shell_output("#{bin}/gfh-keygen")
+    output2 = shell_output("#{bin}/gfh-keygen", 101)
     assert_match "git did not provide -f somehow", output2
   end
 end
