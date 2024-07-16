7f92b5f8db028aa954db2fbd0491f62aa21410f2
Michael Mitchell <ovy@ovyerus.com>
2024-07-16 20:29:57 +1000

    bandsnatch 0.3.2
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/bandsnatch.rb
+++ Formula/bandsnatch.rb
@@ -1,8 +1,8 @@
 class Bandsnatch < Formula
   desc "CLI batch downloader for your Bandcamp collection"
   homepage "https://github.com/Ovyerus/bandsnatch"
-  url "https://github.com/Ovyerus/bandsnatch/archive/refs/tags/v0.3.1.tar.gz"
-  sha256 "976c25c1d29e79c6ff8834725b126e9e7a27f3c7c95c88c78647b67c9aaf3f11"
+  url "https://github.com/Ovyerus/bandsnatch/archive/refs/tags/v0.3.2.tar.gz"
+  sha256 "552cf9b85cb737e35b93e7e7899f883945c280ad9c246f419883e181845a97f0"
   license "MIT"
   head "https://github.com/Ovyerus/bandsnatch.git", branch: "main"
 
